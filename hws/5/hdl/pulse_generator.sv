9ea653a7351ef5c4d5d29d4775be665477e38a92
ianeyk <ian@eykamp.com>
2022-10-24 21:19:53 -0400

    add comments

--- hws/5/hdl/pulse_generator.sv
+++ hws/5/hdl/pulse_generator.sv
@@ -20,12 +20,20 @@ adder_n #(.N(N)) incrementer(.a(counter), .b({N{1'b0}}), .c_in(1'b1), .sum(next_
 comparator_eq #(.N(N)) tick_threshold(.a(ticks), .b(counter), .out(ticks_reached));
 
 always_comb counter_will_be = next_counter | {N{ticks_reached}} | {N{rst}};
+// whenever the ticks_threshold is met or RST is asserted, reset the counter to 111111 on the next tick, 
+// such that next_counter = 000000
+
 // always_comb counter_will_be = next_counter & {N{~ticks_reached}} & {N{~rst}};
+// whenever the ticks_threshold is met or RST is asserted, reset the counter to 000000 on the next tick, 
+// such that next_counter = 000001
+
 always_comb out = ticks_reached & ena;
+// only output if ENA is also enabled
 
 always_ff @(posedge(clk)) begin
-  $display("ticks = %b, counter: %b, ticks_reached: %b", ticks, counter, ticks_reached);
+  // $display("ticks = %b, counter: %b, ticks_reached: %b", ticks, counter, ticks_reached);
   counter <= counter_will_be;
+  // let the next_counter value flow through
 end
 
 
