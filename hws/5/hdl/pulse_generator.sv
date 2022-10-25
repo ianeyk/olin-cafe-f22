83a1aac42614fe8a786379f55c8916d6fb4fd795
ianeyk <ian@eykamp.com>
2022-10-24 21:16:00 -0400

    finish debugging and testing pulse_generator.sv

--- hws/5/hdl/pulse_generator.sv
+++ hws/5/hdl/pulse_generator.sv
@@ -10,17 +10,23 @@ input wire [N-1:0] ticks;
 output logic out;
 
 logic [N-1:0] counter;
-logic ticks_less_than_counter;
+logic [N-1:0] next_counter;
+logic ticks_reached;
 
 logic [N-1:0] counter_will_be;
 logic trash_can;
 
-adder_n #(.N(N)) incrementer(.a(counter), .b({N{1'b0}}), .c_in(1'b1), .sum(counter_will_be), .c_out(trash_can));
-comparator_lt #(.N(N)) tick_threshold(.a(ticks), .b(counter), .out(ticks_less_than_counter));
+adder_n #(.N(N)) incrementer(.a(counter), .b({N{1'b0}}), .c_in(1'b1), .sum(next_counter), .c_out(trash_can));
+comparator_eq #(.N(N)) tick_threshold(.a(ticks), .b(counter), .out(ticks_reached));
 
-// always_ff(@posedge(clk)) begin
-//   counter <= counter_will_be | {N{ticks_less_than_counter}};
-// end
+always_comb counter_will_be = next_counter | {N{ticks_reached}} | {N{rst}};
+// always_comb counter_will_be = next_counter & {N{~ticks_reached}} & {N{~rst}};
+always_comb out = ticks_reached & ena;
+
+always_ff @(posedge(clk)) begin
+  $display("ticks = %b, counter: %b, ticks_reached: %b", ticks, counter, ticks_reached);
+  counter <= counter_will_be;
+end
 
 
 endmodule
