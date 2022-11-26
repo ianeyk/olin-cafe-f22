91d0561a47e881e72c3d96ec8486eb6bf9c93514
ianeyk <ian@eykamp.com>
2022-11-26 16:40:15 -0500

    finish encoding R-typre ALU operations

---
+++ labs/03_risc-v-cpu/hdl/instruction_parse_defines.sv
@@ -0,0 +1,41 @@
+`define RS2_START 24
+`define RS2_END 20
+
+`define RS1_START 19
+`define RS1_END 15
+
+`define FUNCT3_START 14
+`define FUNCT3_END 12
+
+`define FUNCT3_START 31
+`define FUNCT3_END 25
+
+`define OP_START 6
+`define OP_END 0
+
+`define RS1_START 19
+`define RS1_END 15
+
+`define RD_START 11
+`define RS2_END 7
+
+`define I_TYPE_IMM_START 31
+`define I_TYPE_IMM_END 20
+
+`define S_TYPE_IMM_1_START 31
+`define S_TYPE_IMM_1_END 25
+
+`define S_TYPE_IMM_2_START 11
+`define S_TYPE_IMM_2_END 7
+
+`define B_TYPE_IMM_1_START 31
+`define B_TYPE_IMM_1_END 25
+
+`define B_TYPE_IMM_2_START 11
+`define B_TYPE_IMM_2_END 7
+
+`define U_TYPE_IMM_START 31
+`define U_TYPE_IMM_END 12
+
+`define J_TYPE_IMM_START 31
+`define J_TYPE_IMM_END 12
--- labs/03_risc-v-cpu/hdl/rv32i_multicycle_core.sv
+++ labs/03_risc-v-cpu/hdl/rv32i_multicycle_core.sv
@@ -3,6 +3,52 @@
 
 `include "alu_types.sv"
 `include "rv32i_defines.sv"
+`include "instruction_parse_defines.sv"
+
+`define RS2_START 24
+`define RS2_END 20
+
+`define RS1_START 19
+`define RS1_END 15
+
+`define FUNCT3_START 14
+`define FUNCT3_END 12
+
+`define FUNCT7_START 31
+`define FUNCT7_END 25
+
+`define OP_START 6
+`define OP_END 0
+
+`define RS1_START 19
+`define RS1_END 15
+
+`define RD_START 11
+`define RS2_END 7
+
+`define I_TYPE_IMM_START 31
+`define I_TYPE_IMM_END 20
+
+`define S_TYPE_IMM_1_START 31
+`define S_TYPE_IMM_1_END 25
+
+`define S_TYPE_IMM_2_START 11
+`define S_TYPE_IMM_2_END 7
+
+`define B_TYPE_IMM_1_START 31
+`define B_TYPE_IMM_1_END 25
+
+`define B_TYPE_IMM_2_START 11
+`define B_TYPE_IMM_2_END 7
+
+`define U_TYPE_IMM_START 31
+`define U_TYPE_IMM_END 12
+
+`define J_TYPE_IMM_START 31
+`define J_TYPE_IMM_END 12
+
+
+
 
 module rv32i_multicycle_core(
   clk, rst, ena,
@@ -92,7 +138,8 @@ logic [1:0] imm_control;
 immediate_extender imm_extender(.immediate(immediate), .control(imm_control), .out(immediate_extended));
 
 logic [1:0] output_select;
-mux2_32 imm_enabler(.a(reg_data2), .s(imm_enable), .y(alu_src_b));
+logic imm_select;
+mux2_32 imm_enabler(.a({immediate_extended, reg_data2}), .s(imm_select), .y(alu_src_b));
 mux3_32 output_switcher(.a({PC, memory_value, alu_result}), .s(output_select), .y(rfile_wr_data)); // 3, 2, 1 [2:0];
 
 logic [1:0] trash_can;
@@ -101,6 +148,7 @@ adder32 PC_incrementer(.a(PC), .b(32'd4), .cin(1'b0), .s(PC_next), .cout(trash_c
 logic [2:0] instruction_type;
 logic [6:0] op_code;
 always_comb op_code = instruction[6:0];
+
 logic instruction_r_type, instruction_i_type, instruction_l_type, instruction_s_type, instruction_b_type, instruction_u_type, instruction_j_type;
 comparator_eq #(.N(32)) is_r_type(.a(op_code), .b(7'd51), .out(instruction_r_type));
 comparator_eq #(.N(32)) is_i_type(.a(op_code), .b(7'd19), .out(instruction_i_type));
@@ -119,6 +167,7 @@ always_ff @(posedge clk) begin : cpu_controller_fsm
     cpu_controller <= IDLE;
     reg_write <= 0;
     imm_control <= 0;
+    imm_select <= 0;
     immediate <= 20'b0;
     rs1 <= 0;
     rs2 <= 0;
@@ -138,6 +187,7 @@ always_ff @(posedge clk) begin : cpu_controller_fsm
         IDLE : begin // reset all states; everything off
           reg_write <= 0;
           imm_control <= 0;
+          imm_select <= 0;
           immediate <= 20'b0;
           rs1 <= 0;
           rs2 <= 0;
@@ -161,13 +211,55 @@ always_ff @(posedge clk) begin : cpu_controller_fsm
         DONE_LOADING_INSTRUCTION : begin // save instruction in register, so memory can be used for other things
           instruction_store_ena <= 0;
           if (instruction_r_type)
+            cpu_controller <= R_START;
           else if (instruction_i_type)
+            cpu_controller <= I_START;
           else if (instruction_l_type)
+            cpu_controller <= L_START;
           else if (instruction_s_type)
+            cpu_controller <= S_START;
           else if (instruction_b_type)
+            cpu_controller <= B_START;
           else if (instruction_u_type)
+            cpu_controller <= U_START;
           else if (instruction_j_type)
-
+            cpu_controller <= J_START;
+        end
+        R_START : begin // Just a filler state with a standard name.
+          cpu_controller <= R_READ_REGISTERS;
+        end
+        R_READ_REGISTERS : begin
+          rs1 <= instruction[RS1_START:RS1_END];
+          rs2 <= instruction[RS2_START:RS2_END];
+          imm_select <= 0;
+          alu_src_store <= 1; // store the value in the register
+          cpu_controller <= R_ALU;
+        end
+        R_ALU : begin
+          alu_src_store <= 0; // lock the value in the register
+          if      (instruction[FUNCT3_START:FUNCT3_END] == 3'b000 & instruction[FUNCT7_START:FUNCT7_END] == 7'b0000000)
+            alu_control <= ALU_ADD;
+          else if (instruction[FUNCT3_START:FUNCT3_END] == 3'b000 & instruction[FUNCT7_START:FUNCT7_END] == 7'b1000000)
+            alu_control <= ALU_SUB;
+          else if (instruction[FUNCT3_START:FUNCT3_END] == 3'b001)
+            alu_control <= ALU_SLL;
+          else if (instruction[FUNCT3_START:FUNCT3_END] == 3'b010)
+            alu_control <= ALU_SLT;
+          else if (instruction[FUNCT3_START:FUNCT3_END] == 3'b011)
+            alu_control <= ALU_SLTU;
+          else if (instruction[FUNCT3_START:FUNCT3_END] == 3'b100)
+            alu_control <= ALU_XOR;
+          else if (instruction[FUNCT3_START:FUNCT3_END] == 3'b101 & instruction[FUNCT7_START:FUNCT7_END] == 7'b0000000)
+            alu_control <= ALU_SRL;
+          else if (instruction[FUNCT3_START:FUNCT3_END] == 3'b101 & instruction[FUNCT7_START:FUNCT7_END] == 7'b0100000)
+            alu_control <= ALU_SRA;
+          else if (instruction[FUNCT3_START:FUNCT3_END] == 3'b110)
+            alu_control <= ALU_OR;
+          else if (instruction[FUNCT3_START:FUNCT3_END] == 3'b111)
+            alu_control <= ALU_AND;
+          else
+            alu_control <= ALU_INVALID;
+          cpu_controller <= R_WRITE_REGISTERS;
         end
 
       endcase
